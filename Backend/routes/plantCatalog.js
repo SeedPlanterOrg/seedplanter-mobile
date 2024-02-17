9deb13a27ceba0c4661aae94969858ddf4be61ff
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2024-02-17 13:16:33 -0500

    added advanced routing

--- Backend/routes/plantCatalog.js
+++ Backend/routes/plantCatalog.js
@@ -12,4 +12,29 @@ router.post("/", (req, res) => {
     res.status(200).json({message: "recieved JSON file"});
 });
 
+router.put("/", (req, res) => {
+    console.log("Puting in update");
+    console.log("name :" + req.body.name);
+    res.status(200).json({message: "recieved JSON file"});
+});
+
+router.delete("/", (req, res) => {
+    console.log("Deleteing item")
+    res.status(200).json({message: "deleted"});
+});
+router
+    .route("/:id")
+    .get((req, res) => {
+        console.log(`Getting user with id ${req.body.id}`);
+        res.send(`Retreving user with ID: ${req.body.id}`);
+    })
+    .post((req, res) => {
+        console.log(`Getting user with id ${req.body.id}`);
+        res.send(`Posting user with ID: ${req.body.id}`);
+    })
+    .delete((req, res) => {
+        console.log(`Getting user with id ${req.body.id}`);
+        res.send(`Deletinguser with ID: ${req.body.id}`);
+    })
+
 module.exports = router;
--- Frontend/utils/http.js
+++ Frontend/utils/http.js
@@ -1,4 +1,4 @@
 
-function addPlant() {
+function getPlant() {
     
 }
