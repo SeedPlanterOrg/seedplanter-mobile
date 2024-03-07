678093e3044c8c9a819d2e529c1cfb354b6efd9b
Daniel Bergeron <104526289+danbergeron3@users.noreply.github.com>
2024-03-07 10:51:24 -0500

    fix plantCatalog

--- Backend/utils/plantObj.js
+++ Backend/utils/plantObj.js
@@ -2,7 +2,9 @@ class PlantOBJ {
     constructor({id, page, binomial_name, 
         name, daily_watering, zone,
         light, care_level, image_urls, 
-        description}){
+        description, watering_description, 
+        sunlight_description, pruning_description,
+        hardiness_url}){
             this.id = id;
             this.page = page;
             this.binomial_name = binomial_name;
@@ -13,6 +15,10 @@ class PlantOBJ {
             this.care_level = care_level;
             this.image_urls = image_urls;
             this.description = description;
+            this.watering_description =  watering_description,
+            this.sunlight_description = sunlight_description,
+            this.pruning_description = pruning_description,
+            this.hardiness_url = hardiness_url
     }
 };
 
