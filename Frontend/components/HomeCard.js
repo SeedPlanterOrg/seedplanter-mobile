b080275c16b37049a79056c01f7aa34c5287657a
ErsonRamirez21 <erson22mendoza@gmail.com>
2024-04-20 16:29:34 -0400

    Updated homecards with layout similar to the catalog

--- Frontend/components/HomeCard.js
+++ Frontend/components/HomeCard.js
@@ -8,6 +8,8 @@ import {TouchableOpacity} from 'react-native-gesture-handler';
 import { RectButton } from 'react-native-gesture-handler';
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 
+import { FontAwesome6, MaterialCommunityIcons, Feather, AntDesign } from '@expo/vector-icons';
+
 const HomeCard = ({ imageSource, plantName, scientificName, waterLevelProgress, nutrientProgress, onDelete }) => {
     const [modalVisible, setModalVisible] = useState(false);
     const theme = useTheme();
@@ -92,62 +94,161 @@ const HomeCard = ({ imageSource, plantName, scientificName, waterLevelProgress, 
                             transparent={false}
                             visible={modalVisible}
                             onRequestClose={toggleModal}>
-                            <View style={[styles.modalContent, { backgroundColor: theme.gardenCard }]}>
+                            <View style={[styles.modalContent, { backgroundColor: theme.gardenBackground }]}>
                                 {/* Backbutton */}
                                 <View style={styles.Backbutton}>
                                     <Button title="Close" color={theme.text} alignItems="left" onPress={() => setModalVisible(false)}></Button>
                                 </View>
                                 
-                                {imageSource && <Image source={imageSource} style={styles.modalImage} />}
-                                <Text style={styles.modalTitleText}>{plantName}</Text>
-                                <Text style={styles.modalSubText}>{scientificName}</Text>
-                                
-                                {/* Small Note: To make the showsText property work, I had to manually adjust the Circle.js file
-                                in the react-native-progress folder in node_modules. There was a bug in the library where this would
-                                only display 0% no matter what you did.
-                                
-                                It solved by changing
-                                {formatText(progressValue)}
+                                <ScrollView>
+                                    {imageSource && <Image source={imageSource} style={styles.modalImage} />}
 
-                                in  Circle.js (node_modules\react-native-progress\Circle.js) to
+                                    <View style={styles.RightText}>
+                                        <Text style={styles.RightText}>{plantName}</Text>
+                                    </View>
+                                    <View style={styles.sciNameText}>
+                                        <Text style={styles.sciNameText}>{scientificName}</Text>
+                                    </View>
+                                    
+                                    {/* Small Note: To make the showsText property work, I had to manually adjust the Circle.js file
+                                    in the react-native-progress folder in node_modules. There was a bug in the library where this would
+                                    only display 0% no matter what you did.
+                                    
+                                    It solved by changing
+                                    {formatText(progressValue)}
 
-                                {progress ? formatText(progress._value) : this.forceUpdate()} 
-                                */}
-                                
-                                <View style={styles.progressContainer}>
-                                    <View style={styles.progressItem}>
-                                        <Text style={[styles.progressText, { color: '#3FA9EB' }]}>Water</Text>
-                                        <Text style={[styles.progressSubText, { color: '#3FA9EB' }]}>Level</Text>
+                                    in  Circle.js (node_modules\react-native-progress\Circle.js) to
+
+                                    {progress ? formatText(progress._value) : this.forceUpdate()} 
+                                    */}
+                                    
+                                    <View style={styles.progressContainer}>
+                                        <View>
+                                            <Text style={[styles.progressText, { color: '#3FA9EB' }]}>Water</Text>
+                                            <Text style={[styles.progressSubText, { color: '#3FA9EB' }]}>Level</Text>
+                                        </View>
+
+                                        <Progress.Circle
+                                            size={80}
+                                            thickness={10}
+                                            progress={waterLevelProgress}
+                                            color='#3FA9EB'
+                                            borderWidth={0}
+                                            unfilledColor={theme.progBlueUnfill}
+                                            strokeCap='round'
+                                            showsText={true}
+                                            style={{ marginHorizontal: 20 }}
+                                        />
+
+                                        <Progress.Circle
+                                            size={80}
+                                            thickness={10}
+                                            progress={nutrientProgress}
+                                            color='#1DB954'
+                                            borderWidth={0}
+                                            unfilledColor={theme.progUnfill}
+                                            strokeCap='round'
+                                            showsText={true}
+                                            style={{ marginHorizontal: 20 }}
+                                        />
+
+                                        <View>
+                                            <Text style={[styles.progressText, { color: '#1DB954' }]}>Nutrient</Text>
+                                            <Text style={[styles.progressSubText, { color: '#1DB954' }]}>Level</Text>
+                                        </View>
                                     </View>
 
-                                    <Progress.Circle
-                                        size={80}
-                                        thickness={8}
-                                        progress={waterLevelProgress}
-                                        color='#3FA9EB'
-                                        borderWidth={0}
-                                        unfilledColor={theme.progBlueUnfill}
-                                        strokeCap='round'
-                                        showsText={true}
-                                        style={{ marginLeft: 20, marginRight: 20 }}
-                                    />
-                                    <View style={styles.progressItem}>
-                                        <Text style={[styles.progressText, { color: '#1DB954' }]}>Nutrient</Text>
-                                        <Text style={[styles.progressSubText, { color: '#1DB954' }]}>Level</Text>
+                                    {/* Description */}
+                                    <View style={{ alignItems: 'left', justifyContent: 'center', marginLeft: 20, marginRight: 20, marginTop: 10 }}>
+                                        <Text style={[styles.descriptionHeader, { color: theme.text }]}>Description</Text>
+                                        <Text style={[styles.plantDescriptionText, { color: theme.text }]}>N/A</Text>
                                     </View>
 
-                                    <Progress.Circle
-                                        size={80}
-                                        thickness={8}
-                                        progress={nutrientProgress}
-                                        color='#1DB954'
-                                        borderWidth={0}
-                                        unfilledColor={theme.progUnfill}
-                                        strokeCap='round'
-                                        showsText={true}
-                                        style={{ marginLeft: 20, marginRight: 20 }}
-                                    />
-                                </View>
+                                    <View style={{ borderWidth: 0.7, borderColor: theme.text, margin: 40, }} />
+
+                                    <View style={{ justifyContent: 'center', flexDirection: 'row', }} >
+                                        {/* Circle 1 */}
+                                        <View style={[styles.circleContainer, { backgroundColor: theme.gardenBackground, borderColor: "#1DB954"  }]}>
+                                            <Text style={[styles.circleTextHeader, { color: theme.text }]}>N/A</Text>
+                                            <FontAwesome6 name="plant-wilt" size={30} color="#1DB954" />
+                                            <Text style={styles.circleTextSub}>Care Level</Text>
+                                        </View>
+                                        {/* Circle 2 */}
+                                        <View style={[styles.circleContainer, { backgroundColor: theme.gardenBackground, borderColor: "#7EC8E3" }]}>
+                                            <Text style={[styles.circleTextHeader, { color: theme.text }]}>N/A</Text>
+                                            <MaterialCommunityIcons name="water" size={30} color="#7EC8E3" />
+                                            <Text style={styles.circleTextSub}>Daily Watering</Text>
+                                        </View>
+                                    </View>
+
+                                    <View style={{ justifyContent: 'center', flexDirection: 'row', }} >
+                                        {/* Circle 3 */}
+                                        <View style={[styles.circleContainer, { backgroundColor: theme.gardenBackground, borderColor: "#ffd061" }]}>
+                                            <Text style={[styles.circleTextHeader, { color: theme.text }]}>N/A</Text>
+                                            <Feather name="sun" size={30} color="#ffd061" />
+                                            <Text style={styles.circleTextSub}>Sun</Text>
+                                        </View>
+
+                                        {/* Circle 4 */}
+                                        <View style={[styles.circleContainer, { backgroundColor: theme.gardenBackground, borderColor: "#ff7878" }]}>
+                                            <Text style={[styles.circleTextHeader, { color: theme.text }]}>N/A</Text>
+                                            <Feather name="map" size={30} color="#ff7878" />
+                                            <Text style={styles.circleTextSub}>Hardy Zone</Text>
+                                        </View>
+                                    </View>
+
+                                    <View style={{ borderWidth: 0.7, borderColor: theme.text, margin: 40, }} />
+
+                                    {/* Pruing Description */}
+                                    <View style={{ alignItems: 'left', justifyContent: 'center', marginLeft: 20, marginRight: 20, marginTop: 10 }}>
+                                        <View style={{ justifyContent: 'center', flexDirection: 'row', alignItems: 'center', }} >
+                                            <View style={[styles.iconContainer, { borderColor: "#1DB954" }]}>
+                                                <FontAwesome6 name="plant-wilt" size={30} color="#1DB954"/>
+                                            </View>
+                                            <Text style={[styles.descriptionHeader, { color: '#1DB954' }]}>Pruning</Text>
+                                        </View>
+                                        <Text style={[styles.plantDescriptionText, { color: theme.text }]}>N/A</Text>
+                                    </View>
+
+
+                                    {/* Watering Description */}
+                                    <View style={{ alignItems: 'left', justifyContent: 'center', marginLeft: 20, marginRight: 20, marginTop: 10 }}>
+                                        <View style={{ justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }} >
+                                            <View style={[styles.iconContainer, { borderColor: "#7EC8E3" }]}>
+                                                <MaterialCommunityIcons name="water" size={30} color="#7EC8E3" />
+                                            </View>
+                                            <Text style={[styles.descriptionHeader, { color: '#7EC8E3' }]}>Watering</Text>
+                                        </View>
+                                        <Text style={[styles.plantDescriptionText, { color: theme.text }]}>N/A</Text>
+                                    </View>
+
+                                    {/* Sunlight Description */}
+                                    <View style={{ alignItems: 'left', justifyContent: 'center', marginLeft: 20, marginRight: 20, marginTop: 10 }}>
+                                        <View style={{ justifyContent: 'center', flexDirection: 'row', alignItems: 'center' }} >
+                                            <View style={[styles.iconContainer, { borderColor: "#ffd061" }]}>
+                                                <Feather name="sun" size={30} color="#ffd061" />
+                                            </View>
+                                            <Text style={[styles.descriptionHeader, { color: '#ffd061' }]}>Sunlight</Text>
+                                        </View>
+                                        <Text style={[styles.plantDescriptionText, { color: theme.text }]}>N/A</Text>
+                                    </View>
+
+                                    <View style={{ borderWidth: 0.7, borderColor: theme.text, margin: 40, }} />
+
+                                    <View style={{ alignItems: 'center', justifyContent: 'center', marginBottom: 10 }}>
+                                        <Text style={styles.cardText7}>Hardiness Map</Text>
+                                    </View>
+                                    
+                                    {/*
+                                    <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                                        <WebView source={{ uri: plant.hardiness_url }} style={styles.modalImageSizing2} />
+                                    </View>
+                                    */}
+
+                                    <View style={{ margin: 40, }} />
+
+                                </ScrollView>
+                                
                                 
                             </View>
                         </Modal>
@@ -234,12 +335,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
     },
     modalImage: {
-        width: 225,
-        height: 225,
-        borderRadius: 10,
-        marginBottom: 30,
-        marginTop: 30,
-        alignSelf: 'center', 
+        width: '100%',
+        height: 375,
+        resizeMode: 'cover',
+        borderBottomRightRadius: 30,
+        borderBottomLeftRadius: 30,
     },    
     modalTitleText: {
         fontSize: 30,
@@ -257,19 +357,13 @@ const styles = StyleSheet.create({
     },
     progressContainer: {
         flexDirection: 'row',
-        justifyContent: 'flex-start', 
-        alignItems: 'flex-start',
-        marginTop: 10, 
-        marginLeft: 15, 
-    },
-    progressItem: {
+        justifyContent: 'center', 
         alignItems: 'center',
+        marginBottom: 30, 
     },
     progressText: {
         fontSize: 14,
         fontWeight: 'bold',
-        textAlign: 'center', 
-        marginTop: 20,
     },    
     progressSubText: {
         fontSize: 14,
@@ -295,6 +389,99 @@ const styles = StyleSheet.create({
         fontWeight: '500',
         color: "#FFF",
     },
+    RightText: {
+        justifyContent: 'center',
+        fontSize: 30,
+        marginTop: 10,
+        marginLeft: 15,
+        fontWeight: 'bold',
+        color: '#1DB954',
+        textAlign: 'left',
+    },
+    sciNameText: {
+        justifyContent: 'center',
+        fontSize: 15,
+        marginBottom: 20,
+        marginTop: 5,
+        marginLeft: 15,
+        color: '#A8A8A8',
+        textAlign: 'left',
+    },
+    descriptionHeader: {
+        fontSize: 20,
+        marginLeft: 10,
+        color: '#000000',
+        fontWeight: 'normal',
+        textAlign: 'left',
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    plantDescriptionText: {
+        justifyContent: 'center',
+        fontSize: 13,
+        marginBottom: 20,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        color: '#707070',
+        textAlign: 'justify',
+        alignItems: 'center',
+    },
+    circleContainer: {
+        borderRadius: 100,
+        elevation: 3,
+        backgroundColor: '#f2f7fc',
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: '#333',
+        shadowOpacity: 0.2,
+        padding: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowRadius: 2,
+        width: 125,
+        height: 125,
+        marginLeft: 10,
+        marginRight: 10,
+        marginBottom: 20,
+        borderWidth: 2
+      },
+      circleTextHeader: {
+        justifyContent: 'center',
+        fontSize: 16,
+        color: '#000000',
+        textAlign: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        marginBottom: 5,
+      },
+      circleTextSub: {
+        justifyContent: 'center',
+        fontSize: 8,
+        color: '#707070',
+        textAlign: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        marginTop: 5,
+      },
+      iconContainer: {
+        justifyContent: 'center', 
+        alignItems: 'center',
+        borderWidth: 2, 
+        padding: 10, 
+        width: 55, 
+        height: 55, 
+        borderRadius: 20,
+      },
+      cardText7: {
+        marginBottom: 5,
+        justifyContent: 'center',
+        fontSize: 20,
+        color: '#ff7878',
+        fontWeight: 'normal',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginTop: 5,
+      },
 });
 
 export default HomeCard;
